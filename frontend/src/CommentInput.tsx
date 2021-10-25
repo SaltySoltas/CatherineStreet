12f2ae8cf2442f5d9846077fa89905c81eb27b0a
Colin Campbell <colinbc@protonmail.com>
2021-10-25 16:07:33 -0600

    add post to submit button

--- frontend/src/CommentInput.tsx
+++ frontend/src/CommentInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { JsonObjectExpression } from 'typescript';
 
 interface Props {
@@ -8,20 +8,30 @@ interface Props {
 
 export const CommentInput: React.FC<Props> = ({username, sitehash}) => {
 
-    const requestOptions = (content: String) => {
-        return JSON.stringify({
+    const [content, setContent] = useState('');
+
+    const contentDidChange = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log('value: %s', e.target.value);
+        setContent(e.target.value);
+    }
+
+    const requestOptions = () => {
+        console.log(content);
+        return {
             method: 'POST',
             headers: {'Content-Type':'Application/json'},
-            body: {'username':username,'sitehash':sitehash,'content':content}
-        });
+            body: JSON.stringify({'username':username,'sitehash':sitehash,'content':content})
+        };
     }
 
     const onSubmit = () => {
+        fetch("http://localhost:8080/comments/", requestOptions())
         return;
     }
 
     return <div id="commentInput">
-                <input type="text" id="content" placeholder ="What do you have to say about this?"/>
+                <input type="text" id="content" placeholder ="What do you have to say about this?"
+                 value={content} onChange={(e)=>contentDidChange(e)}/>
                 <input type="submit" name="Post" onClick={onSubmit}/>
             </div>
 }
