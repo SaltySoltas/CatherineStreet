2fac6fa30d42dcf03375cfac7bb9dd88b3b94798
Colin Campbell <colinbc@protonmail.com>
2021-10-27 10:11:06 -0600

    wip

---
+++ register.js
@@ -0,0 +1,8 @@
+const tsNode = require('ts-node');
+const testTSConfig = require('./tests/tsconfig.json');
+
+tsNode.register({
+  files: true,
+  transpileOnly: true,
+  project: './tests/tsconfig.json'
+});
