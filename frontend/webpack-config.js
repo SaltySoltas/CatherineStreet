8c85d9d6ff48afb7272018154ec0c7e73e432b16
Colin Campbell <colinbc@protonmail.com>
2021-11-10 11:28:00 -0700

    webpack fix

--- frontend/webpack-config.js
+++ frontend/webpack-config.js
@@ -1,6 +1,6 @@
+const { EnvironmentPlugin, DefinePlugin } = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack-common.js');
-
 
 module.exports = merge(common, {
     mode: "production",
